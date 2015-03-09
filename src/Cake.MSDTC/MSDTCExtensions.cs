a0b4bc49f9a3cfe98f7d13818ce1f73a3cabc3e5
Sergio Silveira <sdsilveira@gmail.com>
2015-03-09 14:33:16 +0000

    first take

---
+++ src/Cake.MSDTC/MSDTCExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+
+namespace Cake.MSDTC
+{
+    [CakeAliasCategory("Web")]
+    public static class MSDTCExtensions
+    {
+        [CakeMethodAlias]
+        public static void StartMSDTC(this ICakeContext context, int timeout = 10)
+        {
+            MSDTCHelper
+               .Using(context)
+               .Start(timeout);
+        }
+
+        [CakeMethodAlias]
+        public static void StopMSDTC(this ICakeContext context, int timeout = 10)
+        {
+            MSDTCHelper
+               .Using(context)
+               .Stop(timeout);
+        }
+
+        [CakeMethodAlias]
+        public static void PauseMSDTC(this ICakeContext context, int timeout = 10)
+        {
+            MSDTCHelper
+               .Using(context)
+               .Pause(timeout);
+        }
+
+        [CakeMethodAlias]
+        public static void ContinueMSDTC(this ICakeContext context, int timeout = 10)
+        {
+            MSDTCHelper
+               .Using(context)
+               .Continue(timeout);
+        }
+    }
+}
---
+++ src/Cake.MSDTC/MSDTCHelper.cs
@@ -0,0 +1,95 @@
+using System;
+using System.ServiceProcess;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+
+namespace Cake.MSDTC
+{
+    internal sealed class MSDTCHelper
+    {
+        private readonly ICakeLog log;
+
+        internal MSDTCHelper(ICakeLog log)
+        {
+            this.log = log;
+        }
+
+        public static MSDTCHelper Using(ICakeContext context)
+        {
+            return new MSDTCHelper(context.Log);
+        }
+
+        public void Start(int timeout = 10)
+        {
+            Activate(
+               ServiceControllerStatus.Running,
+               ServiceControllerStatus.Stopped,
+               service => service.Continue(),
+               "MSDTC started.",
+               "MSDTC already running.",
+               "MSDTC starting...",
+               timeout);
+        }
+
+        public void Stop(int timeout = 10)
+        {
+            Activate(
+               ServiceControllerStatus.Running,
+               ServiceControllerStatus.Stopped,
+               service => service.Continue(),
+               "MSDTC stopped.",
+               "MSDTC already stopped.",
+               "MSDTC stopping...",
+               timeout);
+        }
+
+        public void Pause(int timeout = 10)
+        {
+            Activate(
+                ServiceControllerStatus.Running,
+                ServiceControllerStatus.Paused,
+                service => service.Continue(),
+                "MSDTC paused.",
+                "MSDTC already paused.",
+                "MSDTC pausing...",
+                timeout);
+        }
+
+        public void Continue(int timeout = 10)
+        {
+            Activate(
+                ServiceControllerStatus.Paused,
+                ServiceControllerStatus.Running,
+                service => service.Continue(),
+                "MSDTC resumed.",
+                "MSDTC already running.",
+                "MSDTC resuming...",
+                timeout);
+        }
+
+        private void Activate(
+            ServiceControllerStatus currentStatus,
+            ServiceControllerStatus newStatus,
+            Action<ServiceController> activate, 
+            string success, 
+            string skipped, 
+            string inProgress, 
+            int timeout = 10)
+        {
+            using(var service = new ServiceController("MSDTC"))
+            {
+                if(service.Status == currentStatus)
+                {
+                    this.log.Verbose(inProgress);
+                    activate(service);
+                    service.WaitForStatus(newStatus, TimeSpan.FromSeconds(timeout));
+                    this.log.Information(success);
+                }
+                else
+                {
+                    this.log.Information(skipped);
+                }
+            }
+        }
+    }
+}
